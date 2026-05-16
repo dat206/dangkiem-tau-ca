12d2ca29610080fdf80d692d84f517e2cde80b40
Yorn-F2B <242112663+Yorn-F2B@users.noreply.github.com>
2026-05-16 09:53:42 +0700

    Configure FastAPI backend and CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,13 +6,19 @@ app = FastAPI(
     title="Fishing Vessel Report API"
 )
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(report_router, prefix="/api")
 
